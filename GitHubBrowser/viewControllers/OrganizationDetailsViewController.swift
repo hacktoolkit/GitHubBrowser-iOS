15568f579c7321fcd0977b83678346ce4d3ad6f7
Tommy Dang <quantumventuress@gmail.com>
2014-09-27 14:55:41 -0700

    added table view and rows to organization details view controller

--- GitHubBrowser/viewControllers/OrganizationDetailsViewController.swift
+++ GitHubBrowser/viewControllers/OrganizationDetailsViewController.swift
@@ -8,6 +8,71 @@
 
 import UIKit
 
-class OrganizationDetails: GitHubBrowserViewController {
-   
+class OrganizationDetailsViewController: GitHubBrowserViewController, 
+  UITableViewDataSource, UITableViewDelegate {
+
+  override init(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    title = "Hacktoolkit"
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let mainView = UIView(frame: 
+      CGRect(x: 0, y: 0, 
+        width: CGRectGetWidth(screenRect()), 
+        height: CGRectGetHeight(screenRect())
+      )
+    )
+    view = mainView
+    let tableView = UITableView(frame:
+      CGRect(x: 0, y: 0, 
+        width: CGRectGetWidth(screenRect()), 
+        height: CGRectGetHeight(screenRect())
+      )
+    )
+    tableView.backgroundColor = UIColor.redColor()
+    tableView.dataSource = self
+    tableView.delegate = self
+    view.addSubview(tableView)
+  }
+
+  // Methods
+  struct Repo {
+    var name: String
+    init(name: String) {
+      self.name = name
+    }
+  }
+  func objects() -> [Repo] {
+    var array = [Repo]()
+    for i in 1...10 {
+      let repo = Repo(name: "Repository \(i)")
+      array.append(repo)
+    }
+    return array
+  }
+
+  // Protocols
+  // Protocols - UITableViewDataSource
+
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
+  func tableView(tableView: UITableView, 
+    cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+    let reuseIdentifier = "ReuseIdentifier"
+    let tableViewCell = UITableViewCell(style: UITableViewCellStyle.Value1,
+      reuseIdentifier: reuseIdentifier
+    )
+    tableViewCell.contentView.backgroundColor = UIColor.greenColor()
+    let repo = objects()[indexPath.row]
+    tableViewCell.textLabel?.text = repo.name
+    return tableViewCell
+  }
+  func tableView(tableView: UITableView, 
+    numberOfRowsInSection section: Int) -> Int {
+    return objects().count
+  }
 }
