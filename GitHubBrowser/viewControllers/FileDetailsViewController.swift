84e96fab505484a5bc64be1b7197f7f74aed8c2d
Jonathan Tsai <jontsai@jontsai.com>
2014-09-27 13:15:29 -0700

    add some stuff for user details page

---
+++ GitHubBrowser/viewControllers/FileDetailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FileDetailsViewController.swift
+//  GitHubBrowser
+//
+//  Created by Jonathan Tsai on 9/27/14.
+//  Copyright (c) 2014 Hacktoolkit. All rights reserved.
+//
+
+import UIKit
+
+class FileDetailsViewController: GitHubBrowserViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
