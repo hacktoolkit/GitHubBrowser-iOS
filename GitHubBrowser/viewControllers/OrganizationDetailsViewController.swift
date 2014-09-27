0e26d366fddc7a8a240840bf80f1255fe632ef0e
Tommy Dang <quantumventuress@gmail.com>
2014-09-27 16:03:40 -0700

    created a parent table view controller

--- GitHubBrowser/viewControllers/OrganizationDetailsViewController.swift
+++ GitHubBrowser/viewControllers/OrganizationDetailsViewController.swift
@@ -8,71 +8,61 @@
 
 import UIKit
 
-class OrganizationDetailsViewController: GitHubBrowserViewController, 
-  UITableViewDataSource, UITableViewDelegate {
+class OrganizationDetailsViewController: GitHubBrowserTableViewController {
 
   override init(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
     title = "Hacktoolkit"
   }
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    let mainView = UIView(frame: 
-      CGRect(x: 0, y: 0, 
-        width: CGRectGetWidth(screenRect()), 
-        height: CGRectGetHeight(screenRect())
-      )
-    )
-    view = mainView
-    let tableView = UITableView(frame:
-      CGRect(x: 0, y: 0, 
-        width: CGRectGetWidth(screenRect()), 
-        height: CGRectGetHeight(screenRect())
-      )
-    )
-    tableView.backgroundColor = UIColor.redColor()
-    tableView.dataSource = self
-    tableView.delegate = self
-    view.addSubview(tableView)
-  }
-
   // Methods
-  struct Repo {
+  class RepositoryStruct {
     var name: String
     init(name: String) {
       self.name = name
     }
   }
-  func objects() -> [Repo] {
-    var array = [Repo]()
+  func objects() -> [RepositoryStruct] {
+    var array = [RepositoryStruct]()
     for i in 1...10 {
-      let repo = Repo(name: "Repository \(i)")
+      let repo = RepositoryStruct(name: "Repository \(i)")
       array.append(repo)
     }
     return array
   }
-
-  // Protocols
-  // Protocols - UITableViewDataSource
-
-  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    return 1
+  func objectAtIndexPath(indexPath: NSIndexPath) -> RepositoryStruct {
+    return objects()[indexPath.row]
   }
-  func tableView(tableView: UITableView, 
-    cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 
-    let reuseIdentifier = "ReuseIdentifier"
-    let tableViewCell = UITableViewCell(style: UITableViewCellStyle.Value1,
-      reuseIdentifier: reuseIdentifier
-    )
-    tableViewCell.contentView.backgroundColor = UIColor.greenColor()
-    let repo = objects()[indexPath.row]
-    tableViewCell.textLabel?.text = repo.name
-    return tableViewCell
-  }
-  func tableView(tableView: UITableView, 
-    numberOfRowsInSection section: Int) -> Int {
-    return objects().count
-  }
+    // Protocols
+    // Protocols - UITableViewDataSource
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(tableView: UITableView, 
+        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let reuseIdentifier = "ReuseIdentifier"
+        let tableViewCell = UITableViewCell(style: UITableViewCellStyle.Value1,
+          reuseIdentifier: reuseIdentifier
+        )
+        tableViewCell.contentView.backgroundColor = UIColor.greenColor()
+        tableViewCell.textLabel?.text = objectAtIndexPath(indexPath).name
+        return tableViewCell
+    }
+    override func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+        return objects().count
+    }
+
+    // Protocols - UITableViewDelegate
+    func tableView(tableView: UITableView,
+        didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let repository = objectAtIndexPath(indexPath)
+        let vc = RepositoryDetailsViewController(
+            repository: repository
+        )
+        println(repository.name)
+        navigationController?.pushViewController(vc, animated: true)
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
 }
--- GitHubBrowser/viewControllers/RepositoryDetailsViewController.swift
+++ GitHubBrowser/viewControllers/RepositoryDetailsViewController.swift
@@ -8,28 +8,13 @@
 
 import UIKit
 
-class RepositoryDetailsViewController: GitHubBrowserViewController {
+class RepositoryDetailsViewController: GitHubBrowserTableViewController {
+ var repository: AnyObject
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  init(repository repo: AnyObject) {
+    repository = repo
+    super.init(nibName: nil, bundle: nil)
+    title = repository.name
+    println(repository.name)
+  }
 }
