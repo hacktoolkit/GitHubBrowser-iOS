27ec34ccee055057af7f029db137d08ee818b7ee
Jonathan Tsai <jontsai@jontsai.com>
2014-09-27 13:39:31 -0700

    added OrganizationDetailsViewController and skeleton GitHubClient

---
+++ GitHubBrowser/viewControllers/OrganizationDetailsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OrganizationDetails.swift
+//  GitHubBrowser
+//
+//  Created by Jonathan Tsai on 9/27/14.
+//  Copyright (c) 2014 Hacktoolkit. All rights reserved.
+//
+
+import UIKit
+
+class OrganizationDetails: GitHubBrowserViewController {
+   
+}
