1d25135eedc0e38b63c9d111f705a1769359fc0f
Tommy Dang <quantumventuress@gmail.com>
2014-09-27 19:19:02 -0700

    added tab bar on repo details vc

--- GitHubBrowser/viewControllers/RepositoryDetailsViewController.swift
+++ GitHubBrowser/viewControllers/RepositoryDetailsViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class RepositoryDetailsViewController: GitHubBrowserTableViewController {
+class RepositoryDetailsViewController: GitHubBrowserTableViewController,
+    UITabBarDelegate {
     var descriptionLabel: UILabel?
     var detailsView: UIView?
     var repository: GitHubRepository
     var webView: UIWebView?
+    var tabBar: UITabBar?
 
     init(repository repo: GitHubRepository) {
         repository = repo
@@ -29,20 +31,36 @@ class RepositoryDetailsViewController: GitHubBrowserTableViewController {
 
         detailsView = UIView(frame: screen)
         detailsView!.backgroundColor = UIColor.whiteColor()
-        detailsView!.addSubview(descriptionLabel!)
+        // detailsView!.addSubview(descriptionLabel!)
 
         webView = UIWebView(frame:
             CGRect(
                 x: 0,
                 y: 20 + 44,
                 width: CGRectGetWidth(screen),
-                height: CGRectGetHeight(screen) - (20 + 44)
+                height: CGRectGetHeight(screen) - (20 + 44 + 48)
             )
         )
+        self.detailsView!.addSubview(webView!)
 
-        // self.view.addSubview(detailsView!)
-        self.view.addSubview(webView!)
+        let tab1 = UITabBarItem(title: "Readme", image: nil, tag: 0)
+        let tab2 = UITabBarItem(title: "Files", image: nil, tag: 1)
+        tabBar = UITabBar(frame:
+            CGRect(
+                x: 0,
+                y: CGRectGetHeight(screen) - 48,
+                width: CGRectGetWidth(screen),
+                height: 48
+            )
+        )
+        tabBar!.items = [tab1, tab2]
+        tabBar!.selectedItem = tab1
+        tabBar!.delegate = self
+
+        self.view.addSubview(detailsView!)
         self.mainTableView.hidden = true
+
+        self.view.addSubview(tabBar!)
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -62,5 +80,15 @@ class RepositoryDetailsViewController: GitHubBrowserTableViewController {
         let url     = NSURL(string: string)
         let request = NSURLRequest(URL: url)
         webView!.loadRequest(request)
+    }
+
+    func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem!) {
+        if item.tag == 0 {
+            self.detailsView!.hidden  = false
+            self.mainTableView.hidden = true
+        } else if item.tag == 1 {
+            self.detailsView!.hidden  = true
+            self.mainTableView.hidden = false
+        }
     }
 }
